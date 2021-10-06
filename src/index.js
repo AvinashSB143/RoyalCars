fd0cfa2592cc8d8b1936cfb8bd62c86ddb486416
Avinash <avinash.basavraju@whitehatjr.com>
2021-10-06 10:39:47 +0530

    reducer add on

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import Routing from './Components/routing';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from "./reducers"
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routing />
+        <Provider store={store}>
+          <Routing />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+
+const reducers = (state = {}, action) => {
+    return state;
+}
+
+
+const rootReducer = combineReducers({
+    reducers
+})
+
+export default rootReducer;
+
