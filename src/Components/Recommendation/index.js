33d08aa81384302b0302367033c2fa7d2fca0d4c
gnanavinaym <gnanavinaym@gmail.com>
2021-12-02 12:11:01 +0530

    Added Responsive Css for Recommendation Component

--- src/Components/Recommendation/index.js
+++ src/Components/Recommendation/index.js
@@ -1,19 +1,21 @@
-import { useEffect, useState,useRef  } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./recomendation.css";
 
 const RecommendedCars = () => {
-    const [index, setIndex] = useState(0);
-    const timeoutRef = useRef(null);
-    const cars = ["https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*",
-     "https://cdn.luxe.digital/media/2020/12/15110747/fastest-cars-world-2021-luxe-digital%402x.jpg",
-     "https://cdni.autocarindia.com/ExtraImages/20210610035741_Creta.jpg"];
-    const delay = 2000;
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+  const cars = [
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*",
+    "https://cdn.luxe.digital/media/2020/12/15110747/fastest-cars-world-2021-luxe-digital%402x.jpg",
+    "https://cdni.autocarindia.com/ExtraImages/20210610035741_Creta.jpg",
+  ];
+  const delay = 2000;
 
   const resetTimeout = () => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
-  }
+  };
 
   useEffect(() => {
     resetTimeout();
@@ -30,19 +32,21 @@ const RecommendedCars = () => {
     };
   }, [index]);
 
-    return(
+  return (
     <div className="root_container">
-    <div
+      <div
         className="slideshowSlider"
         style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
       >
-        {cars.map((images, index) => (
-          <div
-            className="slide"
-            key={index}
-            style={{ backgroundImage: `url(${images})` }}
-          ></div>
-        ))}
+        <div className="slidedWrapper">
+          {cars.map((images, index) => (
+            <img
+              className="slideImg"
+              src={images}
+              alt={`Recommendation_${index}`}
+            />
+          ))}
+        </div>
       </div>
 
       <div className="slideshowDots">
@@ -57,8 +61,7 @@ const RecommendedCars = () => {
         ))}
       </div>
     </div>
-    )
-
-}
+  );
+};
 
 export default RecommendedCars;
