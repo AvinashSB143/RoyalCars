b06bf5f96cb84771b399273ddf41bd26eaf8585b
Avinash <avinash.basavraju@whitehatjr.com>
2022-01-13 09:27:58 +0530

    changes for lifestyle cars

--- src/Components/LifeStyle/index.js
+++ src/Components/LifeStyle/index.js
@@ -14,15 +14,15 @@ const LifeStyle = (props) => {
                 Cars by Interests
             </h2>
             <div className="lifeStyle_images">
-                <Link className="lifeStyle_image" to={props.authToken ? "/lifeStyle" : "#"} onClick={() => props.getCarsByCategory("family")}>
+                <Link className="lifeStyle_image" to="/lifeStyle" onClick={() => props.getCarsByCategory("family")}>
                     <img alt="Family" className="lifeStyle_img" src="//d308ljkq6e62o1.cloudfront.net/img/iIOcHsv2Rxy4wmiUJ5VSGg/file.jpeg" />
                     <p className="Choice_Item">FAMILY</p>
                 </Link>
-                <Link className="lifeStyle_image" to={props.authToken ? "/lifeStyle" : "#"} onClick={() => props.getCarsByCategory("adventure")}>
+                <Link className="lifeStyle_image" to="/lifeStyle" onClick={() => props.getCarsByCategory("adventure")}>
                     <img alt="Adventures" className="lifeStyle_img" src="//d308ljkq6e62o1.cloudfront.net/img/mRHuBQXQQ7aI2Qzy68t0eg/file.jpeg" />
                     <p className="Choice_Item">ADVENTURE</p>
                 </Link>
-                <Link className="lifeStyle_image" to={props.authToken ? "/lifeStyle" : "#"} onClick={() => props.getCarsByCategory("value")}>
+                <Link className="lifeStyle_image" to="/lifeStyle" onClick={() => props.getCarsByCategory("value")}>
                     <img alt="Value" className="lifeStyle_img" src="//d308ljkq6e62o1.cloudfront.net/img/ZMEcczt6RE2KfOK5XCiWaA/file.jpeg" />
                     <p className="Choice_Item">VALUE</p>
                 </Link>
